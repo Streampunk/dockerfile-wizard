de1c27fa95654dadcfa7ca04048895ad0d7d357f
scriptorian <scriptorian@streampunk.media>
2019-02-09 08:58:36 +0000

    Remove unwanted sudo

--- scripts/ffmpeg.sh
+++ scripts/ffmpeg.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "RUN sudo apt-get update"
+echo "RUN apt-get update"
 
-echo "sudo add-apt-repository ppa:jonathonf/ffmpeg-4 \
-      && sudo apt-get install libavcodec-dev libavformat-dev libavdevice-dev libavfilter-dev libavutil-dev libpostproc-dev libswresample-dev libswscale-dev"
+echo "add-apt-repository ppa:jonathonf/ffmpeg-4 \
+      && apt-get install libavcodec-dev libavformat-dev libavdevice-dev libavfilter-dev libavutil-dev libpostproc-dev libswresample-dev libswscale-dev"
 
 # delete all the apt list files since they're big and get stale quickly
-echo "RUN sudo rm -rf /var/lib/apt/lists/*"
+echo "RUN rm -rf /var/lib/apt/lists/*"
 # this forces "apt-get update" in dependent images, which is also good
