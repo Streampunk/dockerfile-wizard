9cfad397cbc1a814ba58bb481d994be34feb0c49
scriptorian <scriptorian@streampunk.media>
2019-02-09 09:05:23 +0000

    move ffmpeg install to generate.sh

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -132,3 +132,10 @@ RUN apt-get -y install libgconf-2-4 \
   && mv chromedriver /usr/local/bin/chromedriver \
   && chmod +x /usr/local/bin/chromedriver"
 fi
+
+echo "RUN add-apt-repository ppa:jonathonf/ffmpeg-4 \
+      && apt-get install libavcodec-dev libavformat-dev libavdevice-dev libavfilter-dev libavutil-dev libpostproc-dev libswresample-dev libswscale-dev"
+
+# delete all the apt list files since they're big and get stale quickly
+echo "RUN rm -rf /var/lib/apt/lists/*"
+# this forces "apt-get update" in dependent images, which is also good
